e30d532d008110774ab2fa64abe6cb6dad51f3d0
jfriend00 <jfriend00@users.noreply.github.com>
2020-05-24 01:41:06 -0700

    Add shuffle() to a few tests

--- test/array-test.js
+++ test/array-test.js
@@ -35,6 +35,7 @@ console.log(`maxDeviation = ${maxDeviation.toFixed(3)}%`);
 
 
 let b = new ArrayEx(0,1,2,3,4,5,6,7,8,9);
+b.shuffle();
 b.sortNumeric("descending");
 assert.deepStrictEqual(b, new ArrayEx(9,8,7,6,5,4,3,2,1,0), `sortNumeric descending produced ${JSON.stringify(b)}`);
 console.log('descending numeric sort correct');
@@ -45,6 +46,7 @@ console.log('ascending numeric sort correct');
 
 let c = [0,1,2,3,4,5,6,7,8,9];
 ArrayEx.enhance(c);
+c.shuffle();
 c.sortNumeric("descending");
 assert.deepStrictEqual(c, [9,8,7,6,5,4,3,2,1,0], `sortNumeric descending produced ${JSON.stringify(c)}`);
 console.log('enhanceArray() works');
