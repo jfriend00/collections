258fb6598873ef8f556cee1f1eeddda3e0fe9a00
jfriend00 <jfriend00@users.noreply.github.com>
2020-05-24 00:35:53 -0700

    Add ArrayEx and tests

--- array.js
+++ array.js
@@ -1,7 +1,12 @@
+const { enhance } = require('./utils.js');
+
 class ArrayEx extends Array {
+    // run all requests in parallel, resolve to array of results
     mapParallel(fn, thisArg) {
         return Promise.all(this.map(fn, thisArg));
     }
+
+    // run all requests in series, resolve to array of results
     async mapSeries(fn, thisArg) {
         const newArray = new this.constructor();
         for (const item of this) {
@@ -10,13 +15,16 @@ class ArrayEx extends Array {
         }
         return newArray;
     }
+
+    // run all requests in series, don't keep track of results
     async eachSeries(fn, thisArg) {
         for (const item of this) {
             await fn.call(thisArg, item, item, this);
         }
     }
+
     // not an async method, combines .filter() and .map() in one call
-    // saves an intermediate copy of the array
+    // saves an intermediate copy of the arrayif you chain .filter().map()
     filterMap(fn, thisArg) {
         const newArray = new this.constructor();
         for (const item of this) {
@@ -27,6 +35,46 @@ class ArrayEx extends Array {
         }
         return newArray;
     }
+
+    // randomly order the array using Fisher-Yates Shuffle
+    // this mutates the array in place
+    shuffle() {
+        let index = this.length, randomIndex, temp;
+        while (index > 1) {
+            randomIndex = Math.floor(Math.random() * index);
+            --index;
+            temp = this[index];
+            this[index] = this[randomIndex];
+            this[randomIndex] = temp;
+        }
+    }
+
+    // sort numerically in place
+    // order is ascending or descending
+    sortNumeric(order = "ascending") {
+        const sortFn = order === "ascending" ? (a, b) => a - b : (a, b) => b - a;
+        return this.sort(sortFn);
+    }
+
+    // append the contents of another array onto this one
+    append(array) {
+        const numAdds = array.length;
+        let i = this.length;
+        let j = 0;
+        // grow the array all at once
+        this.length = i + numAdds;
+        while (j < numAdds) {
+            this[i] = array[j];
+            i++;
+            j++;
+        }
+        return this;
+    }
 }
 
-module.exports = { ArrayEx };
+// add ArrayEx methods to a regularArray object
+ArrayEx.enhance = function (regularArray) {
+    return enhance(ArrayEx.prototype, regularArray);
+}
+
+module.exports = { ArrayEx};
---
+++ test/array-test.js
@@ -0,0 +1,55 @@
+const { ArrayEx } = require('../array.js');
+const assert = require('assert').strict;
+
+let a = new ArrayEx(0,1,2,3,4,5,6,7,8,9);
+let len = a.length;
+let results = new Map();
+for (let i = 0; i < len; i++) {
+    results.set(i, (new Array(len)).fill(0));
+}
+
+// const numIterations = 100_000_000;
+const numIterations = 100_000;
+for (let i = 0; i < numIterations; i++) {
+    a.shuffle();
+    // now record the count of where each value fell in the shuffled array
+    for (let j = 0; j < len; j++) {
+        let val = a[j];
+        let data = results.get(val);
+        ++data[j];
+    }
+}
+console.log(`Running ${numIterations} iterations:`);
+let avg = numIterations / len;
+let maxDeviation = 0;
+for (let [key, arr] of results) {
+    let data = arr.map(cnt => {
+        let percent = ((cnt - avg) / avg) * 100;
+        maxDeviation = Math.max(Math.abs(percent), maxDeviation);
+        let percentStr = percent.toFixed(3);
+        return percent >= 0 ? ' ' + percentStr : percentStr;
+    });
+    console.log(`${key}: ${data.join(', ')}`);
+}
+console.log(`maxDeviation = ${maxDeviation.toFixed(3)}%`);
+
+
+let b = new ArrayEx(0,1,2,3,4,5,6,7,8,9);
+b.sortNumeric("descending");
+assert.deepStrictEqual(b, new ArrayEx(9,8,7,6,5,4,3,2,1,0), `sortNumeric descending produced ${JSON.stringify(b)}`);
+console.log('descending numeric sort correct');
+b.sortNumeric("ascending");
+assert.deepStrictEqual(b, new ArrayEx(0,1,2,3,4,5,6,7,8,9), `sortNumeric ascending produced ${JSON.stringify(b)}`);
+console.log('ascending numeric sort correct');
+
+
+let c = [0,1,2,3,4,5,6,7,8,9];
+ArrayEx.enhance(c);
+c.sortNumeric("descending");
+assert.deepStrictEqual(c, [9,8,7,6,5,4,3,2,1,0], `sortNumeric descending produced ${JSON.stringify(c)}`);
+console.log('enhanceArray() works');
+
+let d = new ArrayEx(0,1,2);
+d.append([3,4,5]);
+assert.deepStrictEqual(d, new ArrayEx(0,1,2,3,4,5), `.append() produced ${JSON.stringify(d)}`);
+console.log('.append() works');
